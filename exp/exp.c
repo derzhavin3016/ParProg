97ffb6c2b4d092e231fce4a51f9e399ce41c7fc0
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-03-23 20:28:53 +0300

    [exp] Optimization - Optimized factotial counting (now it calcs inle one time) - Added time measurement via clock()

--- exp/exp.c
+++ exp/exp.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #include <float.h>
 
 typedef unsigned uint;
 typedef long double ldbl;
-
-ldbl fact( uint N );
+enum DATA_TAGS
+{
+  DATA_SUM,
+  DATA_LTERM
+};
 
 void calc_sum(int rank, uint N, int commsize);
 
-ldbl finalize( uint commsize );
+ldbl finalize(uint commsize);
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
   int rank = 0, commsize = 0;
   MPI_Init(&argc, &argv);
@@ -25,29 +29,38 @@ int main( int argc, char *argv[] )
   {
     if (rank == 0)
     {
+      clock_t start = clock();
       ldbl res = finalize(commsize);
+      clock_t end = clock();
+
       printf("e = %.*Lf\n", LDBL_DIG - 1, res);
-      printf("<math.h> M_E = %.*lf\n", DBL_DIG - 1, M_E);
+      printf("M_E = %.*lf\n", DBL_DIG - 1, M_E);
       printf("|e - M_E| = %.*Lf\n", LDBL_DIG - 1, fabsl(res - M_E));
+
+      printf("Executed in %lf s\n", ((double)(end - start)) / CLOCKS_PER_SEC);
     }
     else
       calc_sum(rank, atoi(argv[1]), commsize - 1);
   }
   else if (rank == 0)
-    printf("USAGE: %s AMOUNT_OF_TERMS\n", argv[0]);
+    printf("USAGE: %s NUM_OF_LAST_TERM\n", argv[0]);
 
   MPI_Finalize();
 }
 
-ldbl finalize( uint commsize )
+ldbl finalize(uint commsize)
 {
-  ldbl result = 0.0;
-  for (uint i = 1; i < commsize; ++i)
-  {
-    ldbl i_th_res = 0.0;
-    MPI_Recv(&i_th_res, 1, MPI_LONG_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  ldbl result = 1.0;
+  ldbl cur_fact = 1.0;
 
-    result += i_th_res;
+  for (uint rank = 1; rank < commsize; ++rank)
+  {
+    ldbl cur_res = 0.0, last_term = 0.0;
+    MPI_Recv(&cur_res, 1, MPI_LONG_DOUBLE, rank, DATA_SUM, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(&last_term, 1, MPI_LONG_DOUBLE, rank, DATA_LTERM, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    result += cur_res * cur_fact;
+    cur_fact *= last_term;
   }
 
   return result;
@@ -58,26 +71,18 @@ void calc_sum(int rank, uint N, int commsize)
   ldbl part = 0.0;
 
   uint work_amount = N / commsize;
-  uint start_i = work_amount * (rank - 1);
-  
+  uint start_i = work_amount * (rank - 1) + 1;
+
   if (rank == commsize)
     work_amount += N % commsize;
 
-  ldbl term_i = 1 / fact(start_i);
-  for (uint i = start_i, end_i = start_i + work_amount; i < end_i;)
+  ldbl term = 1.0;
+  for (uint i = start_i, end_i = start_i + work_amount; i < end_i; ++i)
   {
-    part += term_i;
-    term_i /= ++i;
+    term /= i;
+    part += term;
   }
 
-
-  MPI_Send(&part, 1, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
-}
-
-ldbl fact( uint N )
-{
-  ldbl res = 1.0;
-  for (uint i = 1; i <= N; ++i)
-    res *= i;
-  return res;
+  MPI_Send(&part, 1, MPI_LONG_DOUBLE, 0, DATA_SUM, MPI_COMM_WORLD);
+  MPI_Send(&term, 1, MPI_LONG_DOUBLE, 0, DATA_LTERM, MPI_COMM_WORLD);
 }
