9ea500add3ce2e1feafc0f7687fbfd2bc77b0d0b
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-09-09 19:35:10 +0300

    [omp] Finished hello, started sum

--- omp/hello/hello.cc
+++ omp/hello/hello.cc
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <sstream>
 #include <omp.h>
 
 int main()
 {
-  omp_set_num_threads(3);
+  auto max_threads = omp_get_max_threads();
 
 #pragma omp parallel
-  std::cout << "Hello world\n";
+  {
+    unsigned thread_id = omp_get_thread_num();
+    std::stringstream ss;
+    ss << "Hello, I'm thread " << thread_id
+       << " out of " << max_threads << "\n";
+
+    std::cout << ss.str();
+  }
 }
