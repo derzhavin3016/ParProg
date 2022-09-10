19ba1117aab1f861bbf0bc060876fd3b1d4af86b
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-09-11 01:01:14 +0300

    [sum] In progress

--- omp/sum/sum.cc
+++ omp/sum/sum.cc
@@ -1,7 +1,59 @@
+#include <cstdio>
 #include <iostream>
+#include <vector>
+#include <omp.h>
+#include <numeric>
 
+using uint = unsigned;
+using ldbl = long double;
 
-int main(int argc, char *argv[])
+#if !defined(_OPENMP)
+#error "This program requires OpenMP library"
+#endif
+
+ldbl calc_sum(int rank, uint N, int commsize)
 {
+  ldbl part = 0.0;
 
+  uint work_amount = N / commsize;
+  uint start_i = work_amount * (rank - 1) + 1;
+
+  if (rank == commsize)
+    work_amount += N % commsize;
+
+  for (uint i = start_i, end_i = start_i + work_amount; i < end_i; ++i)
+    part += 1.0 / i;
+
+  return part;
+}
+
+int main( int argc, char *argv[] )
+{
+  if (argc < 2)
+  {
+    std::cout << "USAGE: " << argv[0] << " AMOUNT_OF_TERMS\n";
+    return 0;
+  }
+
+  auto num_terms = std::atoi(argv[1]);
+
+  if (num_terms <= 0)
+  {
+    std::cout << "Amount of terms must be positive\n";
+    return 1;
+  }
+
+  auto num_threads = 4;omp_get_max_threads();
+  std::vector<ldbl> parts(num_threads);
+
+#pragma omp parallel num_threads(4)
+  {
+    auto th_id = omp_get_thread_num();
+    parts[th_id] = calc_sum(th_id, num_terms, num_threads - 1);
+    std::cout << parts[th_id] << std::endl;
+  }
+
+  auto sum = std::accumulate(parts.begin(), parts.end(), ldbl(0));
+
+  std::cout << sum << std::endl;
 }
