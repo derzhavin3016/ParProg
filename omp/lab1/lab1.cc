b76091793882bead1e842b387b7a8c8d9877d6f3
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-12-02 15:56:16 +0300

    [omp] Some improvements

--- omp/lab1/lab1.cc
+++ omp/lab1/lab1.cc
@@ -2,7 +2,11 @@
 #include <array>
 #include <cmath>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <string_view>
+
+#include "timer.hh"
 
 constexpr std::size_t ISIZE = 1000;
 constexpr std::size_t JSIZE = 1000;
@@ -19,31 +23,58 @@ void printArr(const ArrTy &arr, std::ostream &ost)
   }
 }
 
-int main(void)
+using ProcFunc = std::function<void(ArrTy &)>;
+
+void processArr(ArrTy &arr)
 {
-  ArrTy a{};
-  std::size_t i = 0, j = 0;
+  for (std::size_t i = 0; i < ISIZE - 1; i++)
+    for (std::size_t j = 6; j < JSIZE; j++)
+      arr[i][j] = std::sin(0.2 * arr[i + 1][j - 6]);
+}
 
-  for (i = 0; i < ISIZE; i++)
-  {
-    for (j = 0; j < JSIZE; j++)
-    {
-      a[i][j] = 10 * i + j;
-    }
-  }
-  for (i = 0; i < ISIZE - 1; i++)
-  {
-    for (j = 6; j < JSIZE; j++)
-    {
-      a[i][j] = std::sin(0.2 * a[i + 1][j - 6]);
-    }
-  }
+void processArrPar(ArrTy &arr)
+{}
 
-  std::ofstream of("result.txt");
+void measureDump(ProcFunc f, ArrTy &arr, std::string_view filename)
+{
+  timer::Timer tim;
+  f(arr);
+  auto elapsed_mcs = tim.elapsed_mcs();
+
+  std::cout << "Elapsed time " << elapsed_mcs / 1000.0l << " ms" << std::endl;
+
+  std::ofstream of(filename.data());
   if (!of)
   {
-    std::cout << "Cannot open file for results" << std::endl;
-    return 1;
+    std::cerr << "Cannot open file for results" << std::endl;
+    return;
   }
-  printArr(a, of);
+  printArr(arr, of);
+}
+
+void initArr(ArrTy &a)
+{
+  // Fill array with data
+  for (std::size_t i = 0; i < a.size(); i++)
+  {
+    auto &ai = a[i];
+    for (std::size_t j = 0; j < ai.size(); j++)
+    {
+      ai[j] = 10 * i + j;
+    }
+  }
+}
+
+int main()
+{
+  ArrTy a{};
+  initArr(a);
+
+#if defined(SEQ_VER)
+  std::cout << "Sequential:" << std::endl;
+  measureDump(processArr, a, "seq.txt");
+#else
+  std::cout << "Parallel:" << std::endl;
+  measureDump(processArrPar, a, "par.txt");
+#endif
 }
